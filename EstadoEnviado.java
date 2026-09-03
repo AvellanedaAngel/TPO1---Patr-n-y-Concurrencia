53bfd62f297ee71c8c9a56c00fb64d7cd9fda7e2
Sebastian Alvarado <sebastian.alvarado@est.fi.uncoma.edu.ar>
2026-09-03 10:28:23 -0300

    Corregi el codigo

--- EstadoEnviado.java
+++ EstadoEnviado.java
@@ -1,10 +1,11 @@
-public class EstadoEnviado implements EstadoPedido { 
+public class EstadoEnviado implements EstadoPedido {
     public void procesar(Pedido pedido) {
-        pedido.setEstado("Pedido: " + pedido.getId() + " entregado");
-        pedido.setEstado(new EstadoEntregado());
+        pedido.mostrarMensaje("Pedido: " + pedido.getId() + " ha sido entregado");
+        pedido.setEstado(new EstadoEntregado()); 
     }
+
     public void cancelar(Pedido pedido) {
-        pedido.setEstado("Pedido: " + pedido.getId() + " cancelado");
-        pedido.setEstado(new EstadoCancelado());
+        pedido.mostrarMensaje("Pedido: " + pedido.getId() + " no se puede cancelar, ya fue enviado");
+       
     }
 }
